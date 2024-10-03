8a2ec63d99d2cbf687bf224755676af860916959
Yi-En <m951235@gmail.com>
2024-10-03 16:43:04 -0700

    Update

--- dist/ex07.js
+++ dist/ex07.js
@@ -1,9 +1,8 @@
+"use strict";
 // Exercise: Write a function `fail` that takes a string message and throws an error with that message.
 // The function should have a `never` return type because it never successfully returns.
-
-function fail(errMessage: string): never {
-    throw new Error(`Uncaught Error: ${errMessage}`)
+function fail(errMessage) {
+    throw new Error(`Uncaught Error: ${errMessage}`);
 }
-
 console.log(fail("Something went wrong"));
 // Expected output: Uncaught Error: Something went wrong
